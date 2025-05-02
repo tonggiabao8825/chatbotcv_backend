8cda0abd9e87c231a56a2e0aaab9e5305fcef503
Tong Gia Bao(BaroDev) <103204842+tonggiabao8825@users.noreply.github.com>
2025-05-02 17:10:12 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,8 +28,7 @@ async def root():
     }
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000)) 
+    port = int(os.environ.get("PORT", 8000))  # Lấy PORT từ môi trường Render
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
-
 
 
